746effa5d1fb18600dc14888b72d1728e2d9980b
saboooor <saboor547@gmail.com>
2023-09-07 23:31:12 -0600

    Initial Commit

---
+++ iOS stuff idk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS stuff idk
+//
+//  Created by Muhammad Saboor Bilal on 2023-09-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS stuff idk/iOS_stuff_idkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_stuff_idkApp.swift
+//  iOS stuff idk
+//
+//  Created by Muhammad Saboor Bilal on 2023-09-07.
+//
+
+import SwiftUI
+
+@main
+struct iOS_stuff_idkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
